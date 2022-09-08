fbe8bafe66000ff7668b4aa5bb50e70d9ea24418
Francisco Deville <frandeville4@gmail.com>
2022-09-08 20:42:25 -0300

    cambios deploy

---
+++ build/precache-manifest.cfd29d50312652796d642246ea8cda5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbf57370269312a5ba3d0c998bc501f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d83a55ec8275f2bf9b97",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "31eeb5a26a88afee3796",
+    "url": "/static/css/main.8653b40b.chunk.css"
+  },
+  {
+    "revision": "d83a55ec8275f2bf9b97",
+    "url": "/static/js/2.9463ebf5.chunk.js"
+  },
+  {
+    "revision": "f39d7cdc6b42de5fba0bb5fecb752384",
+    "url": "/static/js/2.9463ebf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31eeb5a26a88afee3796",
+    "url": "/static/js/main.539197e0.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  }
+]);
